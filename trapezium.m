a476f9b27ac539eb24256fdd743db9e189bd8be5
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-04-03 12:04:07 +0200

    Update trapezium.m

--- trapezium.m
+++ trapezium.m
@@ -3,7 +3,10 @@ function I = trapezium(f,a,b,n)
     x = a:g:b;
     fx = f(x);
     
-    I = 0;
-    for i = 2:size(x)
+    I=0;
+    for i = 2:n+1
         I = I + g*((fx(i-1) + fx(i))/2);
     end
+    
+    
+    
