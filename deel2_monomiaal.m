80cd5c5f4dbba3f26023f6562cca3ddd9ee8b08c
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-04-27 16:59:51 +0200

    click to advance

--- deel2_monomiaal.m
+++ deel2_monomiaal.m
@@ -11,12 +11,23 @@
     %Bereken de fout voor graad i = 1:n
     e = zeros(1,n);
     for i = 1:n
+        k = waitforbuttonpress ;
         %Bereken a_i
         a = A(1:i, 1:i)\b(1:i);
         %bereken de fout ||f - g||
         poly = @(x) polyval(fliplr(a),x);
         e(i) = abs(quad(f,-1,1,1e-8) - quad(poly,-1,1,1e-8));
+        
+        x = linspace(-1,1,100);
+        yf = f(x);
+        yp = poly(x);
+        
+        clf;
+        plot(x,yf,x,yp);
+        
     end
-%d)
     
+%d) Plot de fout ifv de graad. 
+    k = waitforbuttonpress ;
+    plot(e)
     
