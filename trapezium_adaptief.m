981650cac57f32e0d502231b480ae3c02e8be99f
kevinTeugels <kevin.teugels@student.kuleuven.be>
2014-04-30 22:55:17 +0200

    Update trapezium_adaptief.m

--- trapezium_adaptief.m
+++ trapezium_adaptief.m
@@ -1,4 +1,13 @@
 function I = trapezium_adaptief(f,a,b,e )
+    
+    %kijken of een van de eindpunten oneindig is en zo ja dit probleem verhelpen
+    if ~finity(f(a))
+        a=a+eps(a);
+    end
+    if ~finity(f(b))
+        b=b-eps(b);
+    end
+    
     % I1 en I2 berekenen met trapezium-regel met 2 en 3 punten, dit komt neer op de trapezium-regel uitvoeren met 1 en 2 deelintervallen
     t1=trapezium(f,a,b,1);
     t2=trapezium(f,a,b,2);
