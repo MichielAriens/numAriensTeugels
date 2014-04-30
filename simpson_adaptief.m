87fa6cc598dce55302a835cebdc32cd0be3a4a4d
kevinTeugels <kevin.teugels@student.kuleuven.be>
2014-04-30 09:50:39 +0200

    Update simpson_adaptief.m

--- simpson_adaptief.m
+++ simpson_adaptief.m
@@ -1,6 +1,9 @@
 function I = simpson_adaptief(f,a,b,e )
+    % I1 en I2 berekenen met 3 en 5 punten, dit komt neer op simpson toepassen met 2 en 4 deelintervallen
     t1=simpson(f,a,b,2);
     t2=simpson(f,a,b,4);
+    
+    %oplossing berekenen
     if abs(t1-t2)<e
         I=t2;
     else
