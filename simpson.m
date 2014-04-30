87bbf66a6e0f61560616253be212c20f862c268d
kevinTeugels <kevin.teugels@student.kuleuven.be>
2014-04-30 09:45:35 +0200

    Update simpson.m

--- simpson.m
+++ simpson.m
@@ -1,20 +1,27 @@
 function I=simpson(f,a,b,n)
-    g = (b-a)/n; % grootte van een deelinterval
-    x = a:g:b;
+    % grootte van een deelinterval
+    g = (b-a)/n; 
+    
+    %kijken of een van de eindpunten oneindig is en zo ja dit probleem verhelpen
     if ~finity(f(a))
         a=a+eps(a);
     end
     if ~finity(f(b))
         b=b-eps(b);
     end
-    fx = f(x);
-    h = ((b-a)/n)/3;
-    s=0;
     
+    %functiewaarden voor grenzen deelintervallen berekenen
+    x = a:g:b;
+    fx = f(x);
+    
+    %grootte deelintervallen berekenen
+    h = ((b-a)/n)/3;
+    
+    %oplossing berekenen
+    s=0;
     for j = 1:n/2
         i = 2*j + 1;
         s = s + fx(i-2) + 4*fx(i-1) + fx(i);
     end
-    
     I = s*h;
     
