60acba26c5d50c4f6ffd68cddd4e8c192d39ba66
kevinTeugels <kevin.teugels@student.kuleuven.be>
2014-04-30 09:48:42 +0200

    Update trapezium.m

--- trapezium.m
+++ trapezium.m
@@ -1,8 +1,21 @@
 function I = trapezium(f,a,b,n)
-    g = (b-a)/n; % grootte van een deelinterval
+
+    %kijken of een van de eindpunten oneindig is en zo ja dit probleem verhelpen
+    if ~finity(f(a))
+        a=a+eps(a);
+    end
+    if ~finity(f(b))
+        b=b-eps(b);
+    end
+    
+    % grootte van een deelinterval
+    g = (b-a)/n; 
+    
+    %functie evalueren in de grenzen van de deelintervallen
     x = a:g:b;
     fx = f(x);
     
+    %oplossing berekenen
     I=0;
     for i = 2:n+1
         I = I + g*((fx(i-1) + fx(i))/2);
